0c9101dff6bdd4d3527f1c64901dfaa33369a903
NJNJA <brunocanongia@gmail.com>
2014-11-26 02:08:20 -0200

    foto para 400x400

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -2,6 +2,6 @@ class Foto < ActiveRecord::Base
 	belongs_to :produto
 	
 	validates :produto, presence: true
-	has_attached_file :avatar, :styles => { :medium => "300x400>", :thumb => "75x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
